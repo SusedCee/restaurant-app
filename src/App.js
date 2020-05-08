8eeb4c71899276cf8b1c02dbc900a0ba103c9507
Sused Cabrera <susedcabrera@Suseds-MBP.domain>
2020-05-08 12:13:21 -0600

    search and filter state update

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ class App extends Component {
       restaurants: [],
       isLoaded: false,
       selectedState: '',
+      selectedGenre: '',
+      search: '',
     } 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
       headers: {
-      Authorization: 'Api-Key q3MNxtfep8Gt', }, })
+      Authorization: 'Api-Key q3MNxtfep8Gt', }, 
+    })
     .then(res => res.json())
     .then(json => {
       this.setState({
@@ -30,14 +35,12 @@ class App extends Component {
     });
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.name === 'isGoing' ? target.checked : target.value;
-    const name = target.name;
+  handleChange(event) {
+    this.setState({search: event.target.value});
+  }
 
-    this.setState({
-      [name]: value
-    });
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
@@ -47,87 +50,96 @@ class App extends Component {
   if (!isLoaded) {
     return <div> Loading restaurants... </div>
   } else {
-    return (
-      <div className="App">
-        <h1>HUNGR</h1>
-        <p>When hunger kicks in during quarantine, use Hungr to search for a restaurant. </p>
-        <form className='search-form'>
-          <label>
-          Search by name, city or genre
-            <input 
-            className='search-bar' 
-            type='text' 
-            placeholder="e.g: Denver"
-            onChange={this.handleInputChange}
-            />
-            <button
-            className='search-button' 
-            type='submit'
-            > 
-            Search 
-            </button>
-          </label>
-          <br />
-          <label>
-          State
-          <select value={this.state.filteredState}
-              onChange={(e) => this.setState({filteredState: e.target.value})}>
-          {restaurants
-            .sort((a, b) => a.state.localeCompare(b.state))
-            .map(filteredStates => (
-              <option key={filteredStates.value}>
-              {filteredStates.state}
-              </option>
-          ))}
-
-          </select>
-          </label>
-          <label>
-          Genre
-          <select>
-          {restaurants
-            .sort((a, b) => a.genre.localeCompare(b.genre))
-            .map(filteredGenre => (
-              <option>
-              {filteredGenre.genre}
-              </option>
-          ))}
-          </select>
-          </label>
-        </form>
-        <table>
-          <tbody>
-            <tr>
-              <td>Name</td>
-              <td>City</td>
-              <td>State</td>
-              <td>Phone</td>
-              <td>Genre</td>
-            </tr>
-        {restaurants
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((restaurant, id) => (
-            <tr
-            key={restaurant.id}
-            name={restaurant.name} 
-            city={restaurant.city} 
-            state={restaurant.state} 
-            telephone={restaurant.telephone} 
-            genre={restaurant.genre}
-            >
-              <td>{restaurant.name}</td>
-              <td>{restaurant.city}</td>
-              <td>{restaurant.state}</td>
-              <td>{restaurant.telephone}</td>
-              <td>{restaurant.genre}</td>
-            </tr>
-        ))}
-          </tbody>
+      return (
+        <div className='App'>
+          <h1>HUNGR</h1>
+          <p>When hunger kicks in during quarantine, use Hungr to search for a restaurant. </p>
+          <form className='search-form'>
+            <label>
+            Search by name, city or genre
+              <input
+              className='search-bar' 
+              type='text' 
+              value={this.state.value}
+              placeholder="e.g: Studio Cake, Reno, Italian"
+              onChange={this.handleChange}
+              />
+            </label>
+              <button
+              className='search-button' 
+              type='button'
+              onClick={this.handleSubmit}
+              > 
+              Search 
+              </button>
+            <br />
+            <label>
+            State
+              <select 
+              value={this.state.selectedState}
+              onChange={(e) => this.setState({selectedState: e.target.value})}>
+                <option key='all-states'> 
+                All
+                </option>
+                {restaurants
+                  .sort((a, b) => a.state.localeCompare(b.state))
+                  .map(selectedState => (
+                    <option key={selectedState.value} value={selectedState.value}>
+                    {selectedState.state}
+                    </option>
+                ))}
+              </select>
+            </label>
+            <label>
+            Genre
+              <select value={this.state.selectedState}
+                  onChange={(e) => this.setState({selectedGenre: e.target.value})}>
+                <option key='all-states'> 
+                All
+                </option>
+                {restaurants
+                  .sort((a, b) => a.genre.localeCompare(b.genre))
+                  .map(selectedGenre => (
+                    <option key={selectedGenre.value} value={selectedGenre.value}>
+                    {selectedGenre.genre}
+                    </option>
+                ))}
+              </select>
+            </label>
+          </form>
+          <table>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td>City</td>
+                <td>State</td>
+                <td>Phone</td>
+                <td>Genre</td>
+              </tr>
+              {restaurants
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .map((restaurant, id) => (
+                  <tr
+                  key={restaurant.id}
+                  name={restaurant.name} 
+                  city={restaurant.city} 
+                  state={restaurant.state} 
+                  telephone={restaurant.telephone} 
+                  genre={restaurant.genre}
+                  >
+                    <td>{restaurant.name}</td>
+                    <td>{restaurant.city}</td>
+                    <td>{restaurant.state}</td>
+                    <td>{restaurant.telephone}</td>
+                    <td>{restaurant.genre}</td>
+                  </tr>
+              ))}
+            </tbody>
           </table>
 
-      </div>
-    );
-  }
+        </div>
+      );
+    }
   }
 }
 
